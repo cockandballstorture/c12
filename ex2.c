0d13099134fea83aae1b8b1fef93799adb4167f9
cockandballstorture <72618474+cockandballstorture@users.noreply.github.com>
2021-02-25 17:36:20 +0300

    Create ex2.c

---
+++ ex2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+void main(){
+	unsigned char a;
+	int i;
+	int w[8];
+	scanf("%d",&a);
+	for(i=0;i<8;i++){
+		if((a%2)==0){
+			w[7-i]=0;
+		}
+		else{
+			w[7-i]=1;
+		}
+		a=a>>1;
+	}
+	for(i=0;i<8;i++){
+		printf("%d",w[i]);
+	}
+}
