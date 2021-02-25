67a35f32ea9f40c545d7f1bf19f125284005b264
cockandballstorture <72618474+cockandballstorture@users.noreply.github.com>
2021-02-25 18:44:12 +0300

    Update ex2.c

--- ex2.c
+++ ex2.c
@@ -1,19 +1,26 @@
 #include <stdio.h>
 void main(){
 	int a;
-	int i;
-	int w[8];
+	int p,i;
+	int t=2;
+	p=1;
+	int w[p];
 	scanf("%d",&a);
-	for(i=0;i<8;i++){
+	while(a>t){
+			p++;
+			t=2*t;
+		}
+	printf("%d\n",p);
+	for(i=0;i<p;i++){
 		if((a%2)==0){
-			w[7-i]=0;
+			w[p-1-i]=0;
 		}
 		else{
-			w[7-i]=1;
+			w[p-1-i]=1;
 		}
 		a=a>>1;
 	}
-	for(i=0;i<8;i++){
+	for(i=0;i<p;i++){
 		printf("%d",w[i]);
 	}
 }
